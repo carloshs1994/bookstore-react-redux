47ec675bf5aeb1f08fa7bec16e6e295fe782b803
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-02-22 11:28:01 -0600

    Finish Add book to store, render the new book is not ready yet

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import booksReducer from './books/books';
 
-const store = configureStore({
-  reducer: {
-    counter: booksReducer,
-  },
+const reducer = combineReducers({
+  booksReducer,
+  // additional reducers could be added here
 });
+
+const store = createStore(
+  reducer,
+  applyMiddleware(logger),
+);
 
 export default store;
