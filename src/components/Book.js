0da5b6b983f20d039ecd104a36d80bfc3a9602f1
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-02-26 02:11:54 -0600

    Finish style

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,9 +19,44 @@ const Book = ({
         <p className="category">{category}</p>
         <h2>{title}</h2>
         <p className="author">{Author}</p>
-        <button onClick={removeBooksFromStore} id={bookId} type="button">
-          Remove
-        </button>
+        <div className="book-buttons">
+          <button type="button">
+            Comments
+          </button>
+          <span>|</span>
+          <button onClick={removeBooksFromStore} id={bookId} type="button">
+            Remove
+          </button>
+          <span>|</span>
+          <button type="button">
+            Edit
+          </button>
+        </div>
+      </div>
+      <div className="progress">
+        <div>
+          <div className="circle-wrap">
+            <div className="circle">
+              <div className="mask full">
+                <div className="fill" />
+              </div>
+              <div className="mask half">
+                <div className="fill" />
+              </div>
+            </div>
+            <div className="inside-circle" />
+          </div>
+          <div className="progress-value">
+            <h2>66%</h2>
+            <p>Completed</p>
+          </div>
+        </div>
+        <span />
+        <div className="progress-chapter">
+          <p>CURRENT CHAPTER</p>
+          <h3>Chapter 9</h3>
+          <button type="button">UPDATE PROGRESS</button>
+        </div>
       </div>
     </li>
   );
